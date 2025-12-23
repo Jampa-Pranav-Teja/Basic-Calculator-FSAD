459f8b18425cf147d7889b86d08114511ccc8206
Focused-Fool <pranavtejajampa@gmail.com>
2025-12-23 13:40:49 +0530

    added all files

---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a=10, b = 5;
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: " + (a/b));
+    }
+}
