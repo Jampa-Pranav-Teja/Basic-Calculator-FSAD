a3e9fcf85b2a375bbc7c5fe090afc83ddca04997
Focused-Fool <pranavtejajampa@gmail.com>
2025-12-23 14:01:03 +0530

    removed division fault

--- calculator.java
+++ calculator.java
@@ -3,7 +3,6 @@ public class calculator {
         int a=10, b = 5;
         System.out.println("Addition: " + (a+b));
         System.out.println("Multiplication: " + (a*b));
-        System.out.println("Division: " + (a/b));
         System.out.println("Subtraction: " + (a - b));
     }
 }
